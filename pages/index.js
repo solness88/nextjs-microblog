ee7f6aaee52ce9ef9f59b3f8b611c41053e17f75
hirokazu kobayashi <solness.stf@gmail.com>
2022-05-26 21:52:38 +0900

    個別記事のサムネイルなど実装

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,36 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import utilStyle from '../styles/utils.module.css'
 
 export default function Home() {
   return (
     <>
-      <Layout><a href="">drdrdrdrdrdrdr</a></Layout>
+      <Layout>
+        <section>
+          <p>私はフロントエンドエンジニアです。JavaScript/ React/ Next.jsを学習してポートフォリオサイトなどを作成しました。</p>
+        </section>
+        <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
+          <h2>📝エンジニアのブログ</h2>
+          <div className={styles.grid}>
+            <article>
+              <Link href="/">
+                <img src="images/thumbnail01.jpg" alt="" />
+              </Link>
+              <Link href="/">
+                <a>テストタイトル</a>
+              </Link>
+              <br />
+              <small>
+                February 23, 2020
+              </small>
+            </article>
+          </div>
+        </section>
+
+      </Layout>
     </>
   )
 }
+
+
