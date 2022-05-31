c09aebb23e1308582896a4f8c2b07682d5032d98
hirokazu kobayashi <solness.stf@gmail.com>
2022-05-31 19:17:09 +0900

    ローカルでの実装完了

--- pages/index.js
+++ pages/index.js
@@ -2,36 +2,55 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout, { siteTitle } from '../components/Layout'
 import utilStyle from '../styles/utils.module.css'
+import { getPostsData } from '../lib/post'
 
-export default function Home() {
+// SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData();
+  console.log(allPostsData)
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
-    <>
-      <Layout>
-        <section>
-          <p>私はフロントエンドエンジニアです。JavaScript/ React/ Next.jsを学習してポートフォリオサイトなどを作成しました。</p>
-        </section>
-        <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
-          <h2>📝エンジニアのブログ</h2>
-          <div className={styles.grid}>
-            <article>
-              <Link href="/">
-                <img src="images/thumbnail01.jpg" alt="" />
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section>
+        <p>私はフロントエンドエンジニアです。JavaScript/ React/ Next.jsを学習してポートフォリオサイトなどを作成しました。</p>
+      </section>
+      <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({ id, title, date, thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img
+                  src={`${thumbnail}`}
+                  className={styles.thumbnailImage} />
               </Link>
-              <Link href="/">
-                <a>テストタイトル</a>
+              <Link href={`/posts/${id}`}>
+                <a className={utilStyle.boldText}>
+                  {title}
+                </a>
               </Link>
               <br />
-              <small>
-                February 23, 2020
+              <small className={utilStyle.lightText}>
+                {date}
               </small>
             </article>
-          </div>
-        </section>
+          ))}
 
-      </Layout>
-    </>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
